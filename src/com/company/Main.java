a834cfd56283535c9a030480e1bb5b5ecef287e0
Cameron <cameronhanlin@Camerons-MacBook-Pro.local>
2022-02-15 12:44:58 -0700

    Added rounding

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,8 @@
 package com.company;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
 
@@ -9,30 +10,43 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
 
-        double[] temps = new double[7];
+        double[] tempsF = new double[7];
+        double[] tempsC = new double[7];
         double total = 0;
         double average;
+        double celcius;
 
 
-        for(int i=temps.length-1; i>=0; i--){
-            System.out.print("What was the temperature "+i+" days ago? ");
-            temps[i] = scan.nextDouble();
+
+        for(int i=tempsF.length-1; i>=0; i--){ // collect inputs
+            System.out.print("What was the temperature "+i+" days ago in Fahrenheit? ");
+            tempsF[i] = scan.nextDouble();
         }
 
         System.out.println("\b");
         System.out.println("These are all of the numbers you have input: ");
-        for(int i=0; i<temps.length; i++){
-            System.out.print(temps[i]+"  ");
+        for(double x: tempsF){ //print the inputs in F
+            System.out.print(x+"F ");
         }
 
-        for(int i=0; i < temps.length; i++){
-            total = total+temps[i];
+        System.out.println("\b");
+        System.out.println("These are all of the numbers you have input in Celsius: ");
+        for(int i=0; i<tempsF.length; i++){ //convert and fill celsius array
+            celcius = (tempsF[i]-32)/1.8;
+            celcius = Math.round(celcius*10);
+            tempsC[i] = celcius/10;
         }
-        average = total/temps.length;
+        for(double x: tempsC){ //output the temps in C
+            System.out.print(x+"C ");
+        }
 
-        System.out.println("The average temp is "+average);
+        for(int i=0; i < tempsF.length; i++){ // calculates the total of the array in F
+            total = total+tempsF[i];
+        }
+        average = total/tempsF.length;
 
-
+        System.out.println("\b");
+        System.out.println("The average temp is "+average+"F, and "+(average-32)/1.8+"C ");
 
 
     }
